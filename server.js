1ab956e9683b2ba97b75f2ccab466ed8010cba87
arefin5 <arefintalukder5@gmail.com>
2023-05-27 16:33:58 +0600

    fix-some-issue

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-import express, { urlencoded } from 'express';
-const app = express();
-import  main from './routes/main';
+import express from 'express';
+import { urlencoded } from 'express';
+import main from './routes/main';
 import client from './client/client';
+
+const app = express();
+
 // Middleware to parse request bodies
-app.use(urlencoded({ extended: false }));
+app.use(urlencoded({ extended: true }));
 
 // Route to handle the GET request for the form page
 app.get('/', client);
 
 // Route to handle the POST request and process the form data
-app.use('/api',main)
+app.use('/api', main);
+
 // Undefined Route Implement
 app.use("*",(req,res)=>{
-  res.status(404).json({status:"fail",data:"Not Found"})
-})
+  res.status(404).json({status:"fail",data:"Not Found"});
+});
+
 // Start the server
 app.listen(8001, () => {
   console.log('Server is running on http://localhost:8000');
